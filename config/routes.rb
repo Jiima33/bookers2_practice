d552947c1acb0a4b4f6bc36171fe9e40dc7ae281
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-10-08 07:44:38 +0000

    [add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to:"homes#top"
   get "home/about"=>"homes#about"
+  get '/search', to: 'searches#search'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :comments, only: [:create, :destroy]
@@ -13,5 +15,6 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
